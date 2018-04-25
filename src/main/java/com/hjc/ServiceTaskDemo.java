912f3c1a4bd07e17f8193fa2ed04cf9aa654ccf5
'hjc' <'hjc@163.com'>
2018-04-25 14:59:46 +0800

    ServiceTask Filed设置测试

--- src/main/java/com/hjc/ServiceTaskDemo.java
+++ src/main/java/com/hjc/ServiceTaskDemo.java
@@ -1,13 +1,20 @@
 package com.hjc;
 
 import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.Expression;
 import org.activiti.engine.delegate.JavaDelegate;
 
 public class ServiceTaskDemo implements JavaDelegate {
+	
+	private Expression result;
+	
+	private Expression text;
 
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
-		System.out.println("variavles=" + execution.getVariables());  
         execution.setVariable("result", "false");  
+        System.out.println(result.getValue(execution));
+        System.out.println(text.getValue(execution));
+        System.out.println("variavles=" + execution.getVariables()); 
 	}
 }
